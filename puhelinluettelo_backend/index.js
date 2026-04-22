75b97d1038d87c7ba19841195e95b4529d68eab9
heespari <heta.e.parikka@student.jyu.fi>
2026-04-23 01:49:33 +0300

    3.17: uuden numeron päivitys

--- puhelinluettelo_backend/index.js
+++ puhelinluettelo_backend/index.js
@@ -110,6 +110,25 @@ app.delete('/api/persons/:id', (request, response, next) => {
   console.log('Henkilö poistettu')
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end()
+      }
+
+      person.name = name
+      person.number = number
+
+      return person.save().then((updatedPerson) => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch(error => next(error))
+})
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
